a9b54da221fe07987612eb242c6b0c0d88c063af
Kevin <kev776522.mg08@nycu.edu.tw>
2022-07-08 13:53:59 +0800

    try

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const backtestRouter = require('./routes/backtest_route');
 const cors = require('cors');
 const path = require('path');
 
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
